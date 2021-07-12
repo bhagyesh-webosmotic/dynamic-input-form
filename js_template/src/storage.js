f842ad48bf895ddedfce85cd8b751c5ae5a56a66
bhagyesh-webosmotic <bhagyesh.webosmotic@gmail.com>
2021-07-12 13:17:29 +0530

    initial commit before v4

--- js_template/src/storage.js
+++ js_template/src/storage.js
@@ -1,98 +1,97 @@
 class Storage {
-  constructor(sid) {
-    this.sid = sid;
-  }
-  addRow(id, type, value) {
-    if (localStorage['input'] === undefined) {
-      localStorage['input'] = '[]';
-    } else {
-      let dataArray = [];
-      dataArray = JSON.parse(localStorage['input']);
+	constructor(sid) {
+		this.sid = sid;
+	}
+	addRow(id, type, value) {
+		if (localStorage["input"] === undefined) {
+			localStorage["input"] = "[]";
+		} else {
+			let dataArray = [];
+			dataArray = JSON.parse(localStorage["input"]);
 
-      function structure(id, type, value) {
-        this.id = id;
-        this.type = type;
-        this.value = value;
-      }
+			function structure(id, type, value) {
+				this.id = id;
+				this.type = type;
+				this.value = value;
+			}
 
-      const obj = new structure(id, type, value);
+			const obj = new structure(id, type, value);
 
-      dataArray.forEach((element) => {
-        if (element.id == id) {
-          console.log('matched');
-          const index = dataArray.indexOf(element);
-          // console.log(index);
-          dataArray.splice(index, 1);
-        }
-      });
-      dataArray.push(obj);
-      localStorage['input'] = JSON.stringify(dataArray);
-      const main = new Main();
-      main.FM.makeRowGreen(id);
-      // main.FM.activeCheckbox(id)
-    }
-  }
-  removeRow(removeRowId) {
-    let dataArray = [];
-    dataArray = JSON.parse(localStorage['input']);
-    const newArr = dataArray.filter((val) => {
-      return val.id !== removeRowId;
-    });
-    localStorage['input'] = JSON.stringify(newArr);
+			dataArray.forEach((element) => {
+				if (element.id == id) {
+					console.log("matched");
+					const index = dataArray.indexOf(element);
+					// console.log(index);
+					dataArray.splice(index, 1);
+				}
+			});
+			dataArray.push(obj);
+			localStorage["input"] = JSON.stringify(dataArray);
+			const main = new Main();
+			main.FM.makeRowGreen(id);
+			// main.FM.activeCheckbox(id)
+		}
+	}
+	removeRow(removeRowId) {
+		let dataArray = [];
+		dataArray = JSON.parse(localStorage["input"]);
+		const newArr = dataArray.filter((val) => {
+			return val.id !== removeRowId;
+		});
+		localStorage["input"] = JSON.stringify(newArr);
 
-    // for(let i in dataArray){
-    //   if(dataArray[i].id == removeRowId){
+		// for(let i in dataArray){
+		//   if(dataArray[i].id == removeRowId){
 
-    //     let index = dataArray.indexOf(removeRowId)
-    //     dataArray.splice(index, 1)
-    //   }
-    // }
-    // 	localStorage["input"] = JSON.stringify(dataArray);
+		//     let index = dataArray.indexOf(removeRowId)
+		//     dataArray.splice(index, 1)
+		//   }
+		// }
+		// 	localStorage["input"] = JSON.stringify(dataArray);
 
-    const FM = new Form();
-    FM.refreshPage();
-    FM.disableDeleteButtonAfterDelete();
-  }
-  dataRetrieve() {
-    if (localStorage['input'] === undefined) {
-      localStorage['input'] = '[]';
-    } else {
-      let dataArray = [];
-      dataArray = JSON.parse(localStorage['input']);
-      return dataArray;
-    }
-  }
-  checkIfDeletedWasSaved(targetId) {
-    let dataArray = [];
-    dataArray = JSON.parse(localStorage['input']);
-    // console.log(`target id of temp Row:${targetId}`);
-    for (let i in dataArray) {
-      if (dataArray[i].id == targetId) {
-        // console.log("matched temp row for saved data");
-        this.removeRow(targetId);
-      }
-    }
-  }
+		const main = new Main();
+		main.FM.afterRemoveRow();
+	}
+	dataRetrieve() {
+		if (localStorage["input"] === undefined) {
+			localStorage["input"] = "[]";
+		} else {
+			let dataArray = [];
+			dataArray = JSON.parse(localStorage["input"]);
+			return dataArray;
+		}
+	}
+	checkIfDeletedWasSaved(targetId) {
+		let dataArray = [];
+		dataArray = JSON.parse(localStorage["input"]);
+		// console.log(`target id of temp Row:${targetId}`);
+		for (let i in dataArray) {
+			if (dataArray[i].id == targetId) {
+				// console.log("matched temp row for saved data");
+				this.removeRow(targetId);
+			}
+		}
+	}
 
-  deleteSelectedRows(selectedRowIds) {
-    let dataArray = [];
-    dataArray = JSON.parse(localStorage['input']);
+	deleteSelectedRows(selectedRowIds) {
+		let dataArray = [];
+		dataArray = JSON.parse(localStorage["input"]);
 
-    for (let i in selectedRowIds) {
-      for (let j in dataArray) {
-        if (dataArray[j].id == selectedRowIds[i]) {
-          const index = dataArray.indexOf(dataArray[j]);
-          dataArray.splice(index, 1);
-        }
-      }
-    }
-    const dataArrayLength = dataArray.length;
-    localStorage['input'] = JSON.stringify(dataArray);
-    const main = new Main();
-    main.FM.refreshPage();
-    main.FM.disableDeleteButton();
-    if (!dataArrayLength) {
-      main.FM.disableMultiDeleteCheckBox();
-    }
-  }
+		for (let i in selectedRowIds) {
+			for (let j in dataArray) {
+				if (dataArray[j].id == selectedRowIds[i]) {
+					const index = dataArray.indexOf(dataArray[j]);
+					dataArray.splice(index, 1);
+				}
+			}
+		}
+		const dataArrayLength = dataArray.length;
+		localStorage["input"] = JSON.stringify(dataArray);
+		const main = new Main();
+		main.FM.refreshPage();
+		main.FM.disableDeleteButton();
+		if (!dataArrayLength) {
+			main.FM.disableMultiDeleteCheckBox();
+		}
+	}
 }
